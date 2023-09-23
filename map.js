0ae33018f809f147875c2286358894512364fe53
josipcodes <j.potnar@gmail.com>
2023-09-23 12:11:11 +0100

    Write map method examples, solve a challenge

--- map.js
+++ map.js
@@ -5,16 +5,27 @@
  */
 
 // Using a for loop
-
+let nums = [1, 2, 3, 4, 5];
+let results = [];
+for (let num of nums) {
+  results.push(num * 2);
+}
+console.log(results); // [ 2, 4, 6, 8, 10 ]
 
 // Using map()
-
+const multiplyByTwo = function (num) {
+  return num * 2;
+}
+const mapResults = nums.map(multiplyByTwo);
+console.log(mapResults); // [ 2, 4, 6, 8, 10 ]
 
 // Simplified w/ map()
-
+const simplified = nums.map(function(num) {return num * 2});
+console.log(simplified); // [ 2, 4, 6, 8, 10 ]
 
 // Simplfied w/ map() + arrow function
-
+const arrow = nums.map(num => num * 2);
+console.log(arrow); // [ 2, 4, 6, 8, 10 ]
 
 // With objects:
 const students = [
@@ -37,3 +48,6 @@ const students = [
     skill: 'CSS'
   },
 ];
+
+const studentsWithIds = students.map(student => [student.name, student.id])
+console.log(studentsWithIds); // [ [ 'Mark', 1 ], [ 'Ariel', 2 ], [ 'Jason', 3 ] ]
